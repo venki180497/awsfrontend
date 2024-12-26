9b4894c4bf15de9d2ffcc358c920237a4d6be52a
venki180497 <vengat180497@gmail.com>
2024-12-26 16:00:21 +0530

    update

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import React, { useState } from 'react';
 function App() {
   const [data, setData] = useState('');
   const apihandler = async () => {  
-    const response = await fetch('http://13.202.155.38:4000/');
-    const data = await response.text();
+    const response = await fetch('http://13.202.155.38:4000/signup', 
+     { method: 'POST', 
+      headers: { 'Content-Type': 'application/json' }, 
+    body: JSON.stringify({ "email":"venki180497@gmail.com", "password":"123456" })
+  } 
+  );
+    const data = await response.json();
     console.log(data,"data");
-    setData(data);
+    setData(data.message);
   }
   return (
     <div className="App">
